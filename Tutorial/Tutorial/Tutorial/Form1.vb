1a9bff3e59e73cde6978f2b9d423678b869ebffa
Jesal Patel <84318539+JesalMP@users.noreply.github.com>
2021-10-05 02:32:41 +0530

    Add files via upload

---
+++ Tutorial/Tutorial/Tutorial/Form1.vb
@@ -0,0 +1,87 @@
+﻿Imports System.IO
+Imports System.Security
+Imports System.Security.Cryptography
+Public Class Form1
+
+    Function md5_hash(ByVal file_name As String)
+        Return hash_generator("md5", file_name)
+    End Function
+
+    Function hash_generator(ByRef hash_type As String, ByRef file_name As String)
+
+        Dim hash
+        hash = MD5.Create
+
+        Dim hashValue() As Byte
+
+        Dim filestream As FileStream = File.OpenRead(file_name)
+        filestream.Position = 0
+        hashValue = hash.ComputeHash(filestream)
+        Dim hash_hex = PrintByteArray(hashValue)
+
+        filestream.Close()
+
+        Return hash_hex
+    End Function
+
+    Public Function PrintByteArray(ByRef array() As Byte)
+
+        Dim hex_value As String = ""
+
+        Dim i As Integer
+        For i = 0 To array.Length - 1
+
+            hex_value += array(i).ToString("x2")
+        Next i
+
+        Return hex_value.ToLower
+    End Function
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim path As String = OpenFileDialog1.FileName
+            txtFilePath.Text = path
+
+            Dim sample As String
+            sample = md5_hash(path)
+            txtHash.Text = md5_hash(path)
+
+            Using f As System.IO.FileStream = System.IO.File.OpenRead("C:\bhedak project\Tutorial\Tutorial\Tutorial\bin\Debug\md5.txt")
+                Using s As System.IO.StreamReader = New System.IO.StreamReader(f)
+                    While Not s.EndOfStream
+                        Dim line As String = s.ReadLine
+
+                        If (line = sample) Then
+                            lblResult.Text = "Infected!"
+                            lblResult.ForeColor = Color.Red
+                            Dim result As DialogResult = MessageBox.Show("Should we Delete The Threat Found?", "Virus Found", MessageBoxButtons.YesNoCancel)
+                            If result = DialogResult.Cancel Then
+                                MessageBox.Show("Cancel pressed, skipping further operations")
+                            ElseIf result = DialogResult.No Then
+                                MessageBox.Show("No pressed, We have added this file for False-Positive")
+                            ElseIf result = DialogResult.Yes Then
+                                MessageBox.Show("Yes pressed, Threat Has Been Terminated")
+                                FileSystem.Kill(path)
+                            End If
+                        Else
+                            lblResult.Text = "Clean!"
+                            lblResult.ForeColor = Color.Green
+                        End If
+                    End While
+                End Using
+            End Using
+        End If
+    End Sub
+
+    Private Sub lblTitle_Click(sender As Object, e As EventArgs) Handles lblTitle.Click
+
+    End Sub
+
+    Private Sub txtHash_TextChanged(sender As Object, e As EventArgs) Handles txtHash.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ Tutorial/Tutorial/Tutorial/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Tutorial")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Tutorial")>
+<Assembly: AssemblyCopyright("Copyright ©  2015")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ee79a8fb-9ea6-4cc1-8126-7511746feabb")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
